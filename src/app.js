704748d756096421b8875cfedbd7cba0c8fa66f0
Max Carlquist <tenkaklet@gmail.com>
2016-07-03 12:35:18 +0200

    new file

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
 
 var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('<h1>My name is Max</h1>');
+});
+
+app.listen(3000, function () {
+  console.log('Frontend Server Running');
+});
