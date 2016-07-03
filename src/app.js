deaccb41c671da6ddb1c9a2ea5100772a60ad3e6
Max Carlquist <tenkaklet@gmail.com>
2016-07-03 12:46:06 +0200

    added blog route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 
 var express = require('express');
+var posts = require('./mock/posts.json');
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('<h1>My name is Max</h1>');
+});
+
+app.get('/blog', function (req,res) {
+  res.send(posts);
 });
 
 app.listen(3000, function () {
